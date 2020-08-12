f11b91ff4837a606bedab71034a0de77ddcaeb85
brit1172 <baparicio.16@gmail.com>
2020-08-12 15:11:49 -0400

    Add files via upload

--- Finalproject/Finalproject/ViewController.swift
+++ Finalproject/Finalproject/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
-
+import AVFoundation
+var musicEffect: AVAudioPlayer = AVAudioPlayer()
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let musicFile = Bundle.main.path(forResource: "soundfifteenmin", ofType: ".mp3")
+        do{
+            try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath:musicFile!))
+        }
+        catch
+        {
+            print(error)
+        }
+        
     }
 
 
+    
+    
+    @IBAction func noSound(_ sender: Any) {
+        musicEffect.stop()
+    }
+    
+    
+    @IBAction func playSound(_ sender: Any) {
+        musicEffect.play()
+    }
 }
 
