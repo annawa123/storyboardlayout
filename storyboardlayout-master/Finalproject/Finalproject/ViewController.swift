3c627f13339bee6224c98bba4e12ac73f2ed0b2c
suah kim <69254579+suahkim@users.noreply.github.com>
2020-08-12 23:28:56 +0900

    View Controllers Set Up

---
+++ storyboardlayout-master/Finalproject/Finalproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finalproject
+//
+//  Created by Anna Waldbaum on 8/11/20.
+//  Copyright © 2020 Anna Waldbaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
